47bae713d79d047da98e47e4b06199313085dec4
Kamladharan <130892641+Kamladharan@users.noreply.github.com>
2023-05-16 15:48:27 +0530

    Script.py

--- Script.py
+++ Script.py
@@ -7,15 +7,15 @@ All you have to do is add me to a group and give me admin. 😌
 
 I will take care of the rest. 😎"""
 
-    MY_ABOUT_TXT = """★ Server: <a href=https://www.heroku.com>Heroku</a>
+    MY_ABOUT_TXT = """★ Server: <a href=https://www.render.com>Render</a>
 ★ Database: <a href=https://www.mongodb.com>MongoDB</a>
 ★ Language: <a href=https://www.python.org>Python</a>
 ★ Library: <a href=https://pyrogram.org>Pyrogram</a>"""
 
-    MY_OWNER_TXT = """★ Name: Hansaka Anuhas
-★ Username: @Hansaka_Anuhas
-★ ID: <code>5493832202</code>
-★ Country: Sri Lanka 🇱🇰"""
+    MY_OWNER_TXT = """★ Name: Professor 
+★ Username: @jithurg
+★ ID: <code>6056548829</code>
+★ Country: India 🇮🇳"""
 
     STATUS_TXT = """🗂 Total Files: <code>{}</code>
 👤 Total Users: <code>{}</code>
